5f92a91e2e3f36ebdce56779a9cd4deb93d99175
mhebbel2 <mark.hebbel@chainstep.com>
2025-11-24 07:48:46 +0100

    update

--- vastai-load-gpu.sh
+++ vastai-load-gpu.sh
@@ -3,10 +3,7 @@
 ID=$(vastai show instances -q | head -1)
 
 # Copy the selected script to the remote instance
-scp ./loader/* "$(vastai scp-url $ID)" 2>/dev/null || {
-    echo "Error: Failed to copy script to remote instance."
-    exit 1
-}
+scp ./loader/* "$(vastai scp-url $ID)"
 
 # Run the selected script on the remote instance
 ssh $(vastai ssh-url $ID) -- "./run.sh"
